257b01467712e588963be96ea9f69b25647fd07e
Biecho <dimeyer@student.ethz.ch>
2023-11-08 07:37:07 +0000

    Add read physical

--- modules/read_physical/physical_addr_user.c
+++ modules/read_physical/physical_addr_user.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <stdint.h> 
 
 // The ioctl command and structure must match the kernel module
 #define IOCTL_GET_PHY_ADDR _IOR('k', 1, struct vaddr_paddr_conv)
@@ -10,6 +11,45 @@ struct vaddr_paddr_conv {
     unsigned long vaddr;
     unsigned long paddr;
 };
+
+unsigned long get_physical_address_from_pagemap(unsigned long vaddr) {
+    int pagemap_fd;
+    unsigned long paddr = 0;
+    off_t offset;
+    ssize_t bytes_read;
+    const size_t pagemap_entry_size = sizeof(uint64_t);
+
+    pagemap_fd = open("/proc/self/pagemap", O_RDONLY);
+    if (pagemap_fd < 0) {
+        perror("open pagemap");
+        return 0;
+    }
+
+    // Calculate the offset for the virtual address
+    offset = (vaddr / sysconf(_SC_PAGESIZE)) * pagemap_entry_size;
+
+    if (lseek(pagemap_fd, offset, SEEK_SET) == (off_t) -1) {
+        perror("lseek pagemap");
+        close(pagemap_fd);
+        return 0;
+    }
+
+    bytes_read = read(pagemap_fd, &paddr, pagemap_entry_size);
+    if (bytes_read < 0) {
+        perror("read pagemap");
+        close(pagemap_fd);
+        return 0;
+    }
+
+    close(pagemap_fd);
+
+    // Shift the pagemap entry to get the physical address
+    paddr = paddr & ((1ULL << 55) - 1); // Mask out the flag bits
+    paddr = paddr * sysconf(_SC_PAGESIZE); // Get the physical address
+
+    return paddr;
+}
+
 
 int main() {
     int fd;
@@ -43,6 +83,10 @@ int main() {
     }
 
     printf("Virtual Address: %p, Physical Address: %lx\n", buffer, vp.paddr);
+
+    unsigned long paddr_pagemap = get_physical_address_from_pagemap((unsigned long)buffer);
+    printf("Physical Address (pagemap): %lx\n", paddr_pagemap);
+
 
     // Close the device file
     close(fd);
